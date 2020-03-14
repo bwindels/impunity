a000943432fcb10a37efe6ad9bc587d913306e6a
Bruno Windels <brunow@matrix.org>
2020-03-14 21:25:07 +0100

    adjust resolver hook to api changes

--- src/resolve-hook.mjs
+++ src/resolve-hook.mjs
@@ -1,24 +1,31 @@
 const importedPaths = new Set();
+const PATH_URL = "importtracking://paths";
 
 export function resolve(specifier, parentModuleURL, defaultResolver) {
-	if (specifier === "importtracking://paths") {
-		return Promise.resolve({
-			format: 'dynamic',
-			url: specifier
-		});
+	if (specifier === PATH_URL) {
+		return Promise.resolve({url: specifier});
 	}
 	const resolution = defaultResolver(specifier, parentModuleURL);
 	if (global.ImportTrackingConfig) {
 		const config = global.ImportTrackingConfig;
 		if (resolution && resolution.url.startsWith(config.projectDir)) {
 			importedPaths.add(resolution.url);
-			if (config.forceEsm) {
-				resolution.format = "module";
-			}
 		}
 	}
 
 	return Promise.resolve(resolution);
+}
+
+export function getFormat(url, context, defaultFormat) {
+    if (url === PATH_URL) {
+        return Promise.resolve({format: "dynamic"});
+    }
+    const config = global.ImportTrackingConfig;
+    if (config && config.forceEsm && url.startsWith(config.projectDir)) {
+        return Promise.resolve({format: "module"});
+    }
+
+    return defaultFormat(url, context, defaultFormat);
 }
 
 export function dynamicInstantiate() {
