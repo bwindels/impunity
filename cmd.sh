b8b73818371f70b5eebf76d6d4c70086b5f6991e
Bruno Windels <brunow@matrix.org>
2020-08-27 19:24:55 +0200

    update needed node flags

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIR=$(dirname $(realpath -P "$0"))
-node --experimental-modules --experimental-json-modules --loader "$DIR/src/resolve-hook.mjs" "$DIR/src/main.mjs" $@
+node --experimental-json-modules --experimental-loader "$DIR/src/resolve-hook.mjs" "$DIR/src/main.mjs" $@
