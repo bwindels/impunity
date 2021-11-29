803d277c593b6fd57b9e806d47c02230fed9442c
Bruno Windels <bruno@windels.cloud>
2021-11-29 10:44:19 +0100

    add load hook, support node >= 16.12

--- src/resolve-hook.mjs
+++ src/resolve-hook.mjs
@@ -1,14 +1,16 @@
 const importedPaths = new Set();
 const PATH_URL = "data://paths";
+// see importedPathsSource for how source is created
+const PATHS_MODULE_FORMAT = "json";
 
 export {transformSource} from "esbuild-node-loader";
-import {resolve as resolveWithEsBuild, getFormat as getFormatWithEsBuild} from "esbuild-node-loader";
+import {resolve as resolveWithEsBuild, getFormat as getFormatWithEsBuild, load as loadEsBuild} from "esbuild-node-loader";
 
-export function resolve(specifier, context, defaultResolver) {
+export async function resolve(specifier, context, defaultResolver) {
     if (specifier === PATH_URL) {
         return Promise.resolve({url: specifier});
     }
-    const resolution = resolveWithEsBuild(specifier, context, defaultResolver);
+    const resolution = await resolveWithEsBuild(specifier, context, defaultResolver);
     if (global.ImportTrackingConfig) {
         const config = global.ImportTrackingConfig;
         if (config && resolution && resolution.url.startsWith(config.projectDir)) {
@@ -21,23 +23,42 @@ export function resolve(specifier, context, defaultResolver) {
 
 export function getFormat(url, context, defaultFormat) {
     if (url === PATH_URL) {
-        return Promise.resolve({format: "json"});
+        return Promise.resolve({format: PATHS_MODULE_FORMAT});
     }
-    // force .js files to be es modules if configured as such
-    const config = global.ImportTrackingConfig;
-    if (config && url.endsWith(".js") && config.forceEsmDirs.some(dir => url.startsWith(dir))) {
+    if (forceESM(global.ImportTrackingConfig, url)) {
         return Promise.resolve({format: "module"});
     }
     return getFormatWithEsBuild(url, context, defaultFormat);
 }
 
+// force .js files to be es modules if configured as such
+function forceESM(config, url) {
+    return config && url.endsWith(".js") && config.forceEsmDirs.some(dir => url.startsWith(dir));
+}
+
+// the source of PATH_URL
+function importedPathsSource(importedPaths) {
+    return JSON.stringify(Array.from(importedPaths.values()));
+}
+
 export function getSource(url, context, defaultGetSource) {
     if (url === PATH_URL) {
-        const json = JSON.stringify(Array.from(importedPaths.values()));
-
-        return Promise.resolve({source: json});
+        return Promise.resolve({source: importedPathsSource(importedPaths)});
     }
 
     // Defer to Node.js for all other URLs.
     return defaultGetSource(url, context, defaultGetSource);
+}
+
+// New hook starting from Node v16.12.0
+// See: https://github.com/nodejs/node/pull/37468
+export function load(url, context, defaultLoad) {
+    if (url === PATH_URL) {
+        return {format: PATHS_MODULE_FORMAT, source: importedPathsSource(importedPaths)};
+    } else {
+        if (forceESM(global.ImportTrackingConfig, url)) {
+            context.format = "module";
+        }
+        return loadEsBuild(url, context, defaultLoad);
+    }
 }
