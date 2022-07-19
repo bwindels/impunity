87e94b7791ca7f057fee28c5536815a544be8fbd
Bruno Windels <274386+bwindels@users.noreply.github.com>
2022-07-19 15:12:26 +0200

    add example test how to use a commmonjs import (always use a default import from an esm module)

--- example-project/sub/c.js
+++ example-project/sub/c.js
@@ -1,10 +1,12 @@
+import d from "../../other-example-code/d.js";
+
 export async function tests() {
 	await new Promise(r => setTimeout(r, 1000));
 	return {
 		foo: async (assert) => {
 			console.log("hello world!");
 			await new Promise(r => setTimeout(r, 1000));
-			assert(true);
+			assert.equal(d.lol, 7);
 		},
 		bar: assert => {
 			console.log("fail!");
---
+++ other-example-code/d.js
@@ -0,0 +1,1 @@
+module.exports = {lol: 7};
